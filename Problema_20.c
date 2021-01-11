9abc0951fdbcceb0ad0b5a88befbc0dd46a93ca5
Espar <Espar@LAPTOP-MGFK4PO8>
2021-01-11 00:58:27 -0600

    cuadernillo vacaciones 11/01/2021

---
+++ Problema_20.c
@@ -0,0 +1,37 @@
+/* 
+ * File:   Problema_20.c
+ * Author: Espar
+ *
+ * Created on 10 de enero de 2021, 10:41 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    int n=0, x=0;
+    float z=0, p=0;
+    printf("introduce el numero de datos en el que deseas promediar: \n");
+    scanf("%d", &n);
+    float a[n];
+    while(x<n){
+        (x++);
+        printf("introduce el dato, mi prieto amigo: \n");
+        scanf("%f",&a[x]);
+    }
+    x=0;
+    while(x<n){
+        (x++);
+        z=z+a[x];
+     
+    }
+    (p=z/n);
+    
+    printf("%f", p);
+
+    return (EXIT_SUCCESS);
+}
+
