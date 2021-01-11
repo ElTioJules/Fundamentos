1533d61fcb868920180a09d23ce2fb44db1b7351
Espar <Espar@LAPTOP-MGFK4PO8>
2021-01-11 00:57:55 -0600

    cuadernillo vacaciones 11/01/2021

---
+++ Problema_19.c
@@ -0,0 +1,31 @@
+/* 
+ * File:   Problema_19.c
+ * Author: Espar
+ *
+ * Created on 10 de enero de 2021, 10:38 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+      int n = 0, x = 0, y = 1, z = 0, c = 0;
+    printf("ingresar n \n");
+    scanf("%i", &n);
+    while(c<n ){
+        (c++);
+       (x = z + y);
+       (z = y);
+       (y = x);
+       
+       printf("%i, \n", x);
+      
+    }
+    
+
+    return (EXIT_SUCCESS);
+}
+
