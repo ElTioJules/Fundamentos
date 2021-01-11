f938d3528fbbdd6fe77c8294add90bdad2373f8d
Espar <Espar@LAPTOP-MGFK4PO8>
2021-01-11 00:52:43 -0600

    cuadernillo vacaciones 11/01/2021

---
+++ Problema_9.c
@@ -0,0 +1,29 @@
+/* 
+ * File:   Problema_9.c
+ * Author: Espar
+ *
+ * Created on 10 de enero de 2021, 08:36 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    int x=0, y=0;
+    printf("quionda beibi, la verdad me lates un buen, quieres ser mi chiquistriquis?:\n");
+    printf("1 = si            0 = no :C\n");
+    scanf("%i",&x);
+    if(x>0){
+        printf("sabia que dirias que si uwu\n");
+        
+    } else{
+        printf("sabia que aceptarias >:3 \n");
+    } 
+   
+
+    return (EXIT_SUCCESS);
+}
+
