0dd8068d6a1a85f51a603a12467a064b0fc43a93
Espar <Espar@LAPTOP-MGFK4PO8>
2021-01-11 01:00:28 -0600

    cuadernillo vacaciones 11/01/2021

---
+++ Problema_25.c
@@ -0,0 +1,47 @@
+/* 
+ * File:   Problema 25.c
+ * Author: Espar
+ *
+ * Created on 10 de enero de 2021, 11:30 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+     int x=0, n=0, f=0;
+    printf("metele el numero total de datos papu: \n");
+    scanf("%i",&n);
+    int a[n];
+    for(x=0;x<n;x++){
+        printf("introduce el dato wey: \n");
+        scanf("%i",&a[x]);   
+    }
+    for(int y=0;y<n;y++){
+        for(int z=0;z<n;z++){
+            if(a[z]>a[z+1]){
+                f=a[z+1];
+                a[z+1]=a[z];
+                a[z]=f;
+                
+                
+            }
+        }
+    }
+    for(int y=n ;y>0;y--){
+        
+        printf("%i\n", a[y]);
+     
+    }
+    
+
+
+    
+    
+
+    return (EXIT_SUCCESS);
+}
+
