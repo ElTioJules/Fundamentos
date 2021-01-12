9e05aea579d354bbae31d8e0dde00d18a8e68e6b
Espar <Espar@LAPTOP-MGFK4PO8>
2021-01-11 19:55:50 -0600

    actividad en clase 11/01/2020

---
+++ Cadena_arrays.c
@@ -0,0 +1,23 @@
+/* 
+ * File:   Cadena_arrays.c
+ * Author: Espar
+ *
+ * Created on 11 de enero de 2021, 07:25 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    char nombre[21];
+    printf("escribe tu primer nombre plotz \n");
+    scanf("%s", &nombre);
+    printf(" a puesto a que tu nombre es : %s has flipao, eh", nombre);
+    
+
+    return (EXIT_SUCCESS);
+}
+
